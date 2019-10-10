109e8f1c6d64e942c098c97c5e8ea07e26521e0e
Grisha Sergeev <ghostxwheel@gmail.com>
2019-10-10 23:42:58 +0300

    Added TimePicker with input[type=tel] element (phone usage) fixed

--- ui/src/control/TimePicker.js
+++ ui/src/control/TimePicker.js
@@ -1,14 +1,14 @@
 sap.ui.define([
-  'jquery.sap.global',
   'sap/m/TimePicker',
-  'sap/ui/Device',
-  // 'com/ui5/dotproject/timecard/thirdparty/jquery.mask.min',
+	'sap/ui/core/EnabledPropagator',
+  'sap/m/MaskEnabler',
+  'jquery.sap.global',
   'com/ui5/dotproject/timecard/thirdparty/cursorPos'
 ],
-  function (jQuery, TimePicker, Device) {
+  function (TimePicker, EnabledPropagator, MaskEnabler) {
     "use strict";
 
-    var CTimePicker = TimePicker.extend("com.ui5.dotproject.timecard.control.TimePicker", {
+    var CTimePicker = TimePicker.extend("com.ui5.dotproject.timecard.control.TimePicker", /** @lends com.ui5.dotproject.timecard.control.TimePicker.prototype */ {
       renderer: {},
       
       onAfterRendering: function () {
@@ -16,12 +16,19 @@ sap.ui.define([
       }
     });
 
-    CTimePicker.prototype.onfocusin = function(oEvent) {
-      if (TimePicker.prototype.onfocusin) {
-        TimePicker.prototype.onfocusin.apply(this, arguments);
-      }
+		EnabledPropagator.call(CTimePicker.prototype, true);
+		MaskEnabler.call(CTimePicker.prototype);
+
+		CTimePicker.prototype.init = function() {
+      TimePicker.prototype.init.apply(this, arguments);
     };
+    
+    CTimePicker.prototype.setValue = function(sValue) {
+      if (TimePicker.prototype.setValue) {
+        TimePicker.prototype.setValue.apply(this, arguments);
+      }
+			return this;
+		};
 
     return CTimePicker;
-
-  }, /* bExport= */ true);
+  });
