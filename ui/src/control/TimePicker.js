8fe23470880a16799c75d34dcaef7eee035ca216
Grisha Sergeev <ghostxwheel@gmail.com>
2019-10-10 22:26:23 +0300

    Added TimePicker with input[type=tel] element (phone usage)

--- ui/src/control/TimePicker.js
+++ ui/src/control/TimePicker.js
@@ -1,36 +1,26 @@
 sap.ui.define([
   'jquery.sap.global',
-  'sap/m/Input',
+  'sap/m/TimePicker',
   'sap/ui/Device',
-  'com/ui5/dotproject/timecard/thirdparty/jquery.mask.min'
+  // 'com/ui5/dotproject/timecard/thirdparty/jquery.mask.min',
+  'com/ui5/dotproject/timecard/thirdparty/cursorPos'
 ],
-  function (jQuery, Input, Device) {
+  function (jQuery, TimePicker, Device) {
     "use strict";
 
-    var CTimePicker = Input.extend("com.ui5.dotproject.timecard.control.TimePicker", {
+    var CTimePicker = TimePicker.extend("com.ui5.dotproject.timecard.control.TimePicker", {
       renderer: {},
       
       onAfterRendering: function () {
         this._$input.attr("type", "tel");
-        this._$input.mask("99:99");
       }
     });
 
     CTimePicker.prototype.onfocusin = function(oEvent) {
-      if (Input.prototype.onfocusin) {
-        Input.prototype.onfocusin.apply(this, arguments);
+      if (TimePicker.prototype.onfocusin) {
+        TimePicker.prototype.onfocusin.apply(this, arguments);
       }
-      
-      this._$input.select();
     };
-
-    CTimePicker.prototype.onkeydown = function (oEvent) {
-      if (Input.prototype.onkeydown) {
-        Input.prototype.onkeydown.apply(this, arguments);
-      }
-
-      this.setValue(this._$input.val());
-    }
 
     return CTimePicker;
 
